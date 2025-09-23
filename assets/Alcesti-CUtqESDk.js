aabfb7952be9263a880259d5c6ed0139a72b473b
sabareesh-h <sabareesh-h@users.noreply.github.com>
2025-09-23 15:36:10 +0000

    deploy: efe4d2c95f8f7390fcc1aff83e88f43b26b334f3

---
+++ assets/Alcesti-CUtqESDk.js
@@ -0,0 +1,7 @@
+const s=`---
+name: 'Alcestis'
+date: '2025-12-12'
+image: 'https://images.unsplash.com/photo-1579546929518-9e396f3cc809?crop=entropy&cs=tinysrgb&fit=max&fm=jpg&ixid=M3w3Nzg4Nzd8MHwxfHNlYXJjaHwxfHxhbGNlc3Rpc3xlbnwxfHx8fDE3NTg0NjY5MDB8MA&ixlib=rb-4.1.0&q=80&w=1080'
+---
+
+asdfasd lkjas df`;export{s as default};
