4ea1508535b2de1a7ff3c346d016ff9089e71bdb
sabareesh_h <sabarigirimana123@gmail.com>
2025-09-23 20:28:03 +0530

    Updates

---
+++ assets/Alcesti-Ba0mAHQz.js
@@ -0,0 +1,7 @@
+const s=`---\r
+name: 'Alcestis'\r
+date: '2025-12-12'\r
+image: 'https://images.unsplash.com/photo-1579546929518-9e396f3cc809?crop=entropy&cs=tinysrgb&fit=max&fm=jpg&ixid=M3w3Nzg4Nzd8MHwxfHNlYXJjaHwxfHxhbGNlc3Rpc3xlbnwxfHx8fDE3NTg0NjY5MDB8MA&ixlib=rb-4.1.0&q=80&w=1080'\r
+---\r
+\r
+asdfasd lkjas df`;export{s as default};
---
+++ assets/my-first-drawing-DSF_tb-H.js
@@ -0,0 +1,7 @@
+const r=`---\r
+name: 'My First Drawing'\r
+date: '2025-09-23'\r
+image: 'https://images.unsplash.com/photo-1541701494587-cb58502866ab?crop=entropy&cs=tinysrgb&fit=max&fm=jpg&ixid=M3w3Nzg4Nzd8MHwxfHNlYXJjaHwxfHxhYnN0cmFjdHxlbnwxfHx8fDE3NTg0NjY3MDB8MA&ixlib=rb-4.1.0&q=80&w=1080'\r
+---\r
+\r
+This is my first drawing.`;export{r as default};
