cf357e4bfd9ab3aa05344e153559eb7e87463180
sabareesh-h <sabareesh-h@users.noreply.github.com>
2025-10-31 14:54:03 +0000

    deploy: 1f0c413393a5d0d8dcda3f7f41bdb0b64ae7d0b9

---
+++ assets/why-we-cant-choose-something-when-we-get-overwhelm-DuBevgAx.js
@@ -0,0 +1,29 @@
+const e=`---
+title: 'Why we can't choose something, when we get overwhelmed with choices'
+date: '2025-10-31'
+excerpt: 'This is my blog about paradox of choices'
+image: 'https://upload.wikimedia.org/wikipedia/commons/thumb/c/cd/Ambrogio_Lorenzetti_-_Allegory_of_Good_Government_-_Google_Art_Project.jpg/1200px-Ambrogio_Lorenzetti_-_Allegory_of_Good_Government_-_Google_Art_Project.jpg?20130108112130'
+category: 'adsf'
+readTime: '5 min read'
+---
+
+When you want to buy or get something from the place. If you have lower choice, you can easily identify which one do you need. But if you get lot of options with it, you will feel overwhelmed. Why that happens.
+
+Around when i was studying about investing to invest in stocks for the first time. Started feeling that overwhelming feeling. I was fixed to buy a nifty 50 index fund, Because it was stable and most reliable. And also less charges compared to other Mutual funds. 
+
+Then, I found out there are multiple MF that are performing well compared to nifty 50. So I searched about it. And then i found more than 3000 MF with better return. 
+
+I went into the paradox of choices. That is, you have lesser choice you can easily identify which one you need. But when you given more than needed choice. You can't find which one do you need. Even though, you have enough choice, you can't make find the one you want. what a paradox!
+
+The reason why that happens is that, through out evolution, our brain will try to find the one you need through which one will work best for you. But when you are giving more options, your brain will start to think, may be it is not needed much. Because things are enough for now. 
+
+But today's world economy doesn't not work like that. We have more options for every products than our ancestors. Every company is trying to get their product to make profits. And that is good for people, Because the quality increases.
+
+The only way i found to find the one you need is to do research. When i started researching about other mutual funds. I finally came to the conclusion to buy the one i need most. 
+
+How i researched it is, by mostly watching videos and chart comparison. Now, I am a beginner, so i don't know much about finding the right stock. I just simply compared every Mutual funds with mutual funds for the last 5 years. And then found the one. And also i gained a lot of knowledge through that. 
+
+So if i want to buy a mutual fund. I will feel less worried about choices. Because, i'll know how to do research. 
+
+Everywhere we go, there will be enough option for you to choose. But when you know what is best. you can simply get that with satisfaction.
+`;export{e as default};
