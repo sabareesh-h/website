9ea85582e11438d117207bf6c1252b4c78340cea
sabareesh-h <sabareesh-h@users.noreply.github.com>
2025-11-06 14:59:35 +0000

    deploy: 281e7cf0c45bf782b46a34ef57e2b466618b0907

---
+++ assets/the-world-has-a-changed-a-lot-in-the-50-years-CnicRJR1.js
@@ -0,0 +1,27 @@
+const e=`---
+title: 'The world has a changed a lot in the 50 years'
+date: '2025-11-06'
+excerpt: 'Technological advancements in the last 50 years.'
+image: 'https://gertitashkomd.com/wp-content/uploads/2025/05/Italian-Renaissance-Begins-c1350%E2%80%931550.jpg'
+category: 'Learning'
+readTime: '5 min read'
+---
+
+When i was sitting class and got bored, i let my mind wander and came to know that all the things around me are mostly invented within the last 50 year or so. Not far, We grown so fast in the last 50 than hundreds of year.
+
+I wondered why, maybe lot of intelligent people are born around this time, or because of two world wars or maybe we stop worry about food and survival, so that we start inventing stuff to cure our boredom. 
+
+I'd say it's because of the connective and collective mind we all have access to. When technological advancement booms, the information are flowing so fast and people building things upon them to make the thing even more efficient. 
+
+Let's take a car for example, It started 100 years ago to make people move around easily using petrol. And companies are created to extract petrol, and sell it to the consumer, and car manufacturing companies started manufacturing faster due to demand. And made car to become much cheaper. Because the more you create, the cheaper it becomes to supply, because of return on investment. For example, when you buy a machine that makes car. And car are produced at much faster level, you buy more machine that makes car. But you need to pay the machine every time a new car was created, that's what return on investment is. Create a car faster and make it cheaper to create. 
+
+So it means that knowledge of making car spreads faster and competition comes and then, that makes the companies making cars feel a need to innovative to be in lead all the time. That's when technology grows faster. Because all the companies are fighting to get consumer buy the car they sell. They do it by telling us all the innovative things their has.
+
+In a way, it is good for us, we can enjoy all these things if we have money. And also it awe me, we went from villages to sky scraper reaching the sky, from horse riding to flying in plane, from having to buy different things to buy a smart phone have all that one place, from moving around to know the information to access all on our phone. 
+
+What a technology smart phone, nowadays, most of the things you want cacn be done by smart phone. And i feel excited to experience AI as well. 
+
+I learnt that word reconnaissance, it's a ancient rome word, that means a polymath. As a polymath myself, when AI started to boom, i felt like the world change forever once it is fully combined into our world. It can do all the mundane things i make free to do other things. 
+
+I finally saw now, we are simply in one of the most comfortable and exciting compared to the time before.   
+`;export{e as default};
