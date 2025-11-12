7f632c14e96b45a817b2500f42a73731d6053444
sabareesh-h <sabareesh-h@users.noreply.github.com>
2025-11-12 15:08:23 +0000

    deploy: 334c2a0eb727764e7399c28b46ca6db8f4a08d4c

---
+++ assets/the-time-that-made-me-who-i-am-today-Dw4GMIwI.js
@@ -0,0 +1,32 @@
+const e=`---
+title: 'The time that made me who I am today'
+date: '2025-11-12'
+excerpt: 'This is a part of life story that struggled a lot and as an outcome, I learned a lot. '
+image: 'https://bigthink.com/wp-content/uploads/2023/09/Albrecht_Altdorfer_-_Schlacht_bei_Issus_Alte_Pinakothek_Munchen.jpg'
+category: 'Learning'
+readTime: '5 min read'
+---
+
+In 2024, My college started the placement process by getting company from all around the Tamil Nadu. The reason why i joined this college is because of this college reputation of getting us placed in most of the good company if we did well. 
+
+And I did well in studies, I got a CPGA of 8.9 out of 10. Which was way higher than most of the class. I am the fourth rant student of my department. Still at the start of the placement interview, I was kind of started struggling. Because my English speaking skill is not every good. Even though I have some good skill in coding as well as projects in both coding and mechanical engineering related projects. 
+
+Even then, I was struggling to the full. I remember every failure that made me more stress out and depressed. I have a memory me and another guy, who is experiencing the same problem of getting placed when other around getting placed, is sitting in some placement office another college, staring everyone who got placed wonder, how they did even when I did more than enough to get placed. I went to attend interviews to different colleges and still not getting placed. I became hopeless. And I was getting too stress out and depressed when placement season is coming to an end. 
+
+In december, I attended TCS and the interviewer i got is so problematic, he straight up rejected me when i said i am study mechanical engineer. And most of the friends got a job at the time. Some are going to join the company as an intern at the end of december. 
+
+I was getting more and more stressed as well as depressed. And then Delphi TVS started recruiting and I attended it and went again to the city. I finally got placed. The relief I felt at that time so massive. I felt like whole life has been saved. 
+
+Before I got placed, I was anxious to go there, because my relations member will start giving me advice, that I don't want to hear. So i didn't attend them.
+At the start of january I started attending family gathering, because now i have a job. And i am free of those questions.
+
+And joined the company in February, Now whenever new problem arises, I still think about the days I gone through, the tough times of not being enjoy anything but think about getting placed. That made me to not be afraid of things right now. That time is surely a hell.  
+
+But I made as a person who is confident than before and not getting scared of uncertain. Surely when you out of hell, you will be a different person in a good way. 
+
+Now I am currently pursuing the peaceful life, before that time made me yearn for peace. I won't give up the peace that i am currently holding, which earned through living in hell.
+
+One thing I learned about life is, whatever happens, It will end in good way. Now, the company I didn't get placed is not giving salary to people who placed. So I escaped, but I feel sadness for the people who is not getting a salary. Hopefully, they will get sooner. 
+
+
+`;export{e as default};
